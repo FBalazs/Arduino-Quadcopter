664357d56953f5bdb3a60def0131dda0c3845426
Tegzes Tamás <tegzes.tamas@gmail.com>
2015-07-23 16:56:13 +0200

    Motor control with servolib

---
+++ Quadcopter/Motors.ino
@@ -0,0 +1,21 @@
+#define MOTOR_PIN_FL 9
+#define MOTOR_PIN_FR 10
+#define MOTOR_PIN_BL 11
+#define MOTOR_PIN_BR 12
+
+Servo motorFL, motorFR, motorBL, motorBR;
+
+void initMotors(){
+  motorFL.attach(MOTOR_PIN_FL);
+  motorFR.attach(MOTOR_PIN_FR);
+  motorBL.attach(MOTOR_PIN_BL);
+  motorBR.attach(MOTOR_PIN_BR);
+}
+
+void setMotors(int pwFL,pwFR,pfBL,pwBR){
+  motorFL.write(pwFL);
+  motorFR.write(pwFR);
+  motorBL.write(pwBL);
+  motorBR.write(pwBR);
+}
+
