ae3059ec9b6eb1ef0485a27a6cc4ea926eb3d2d4
Tegzes Tamás <tegzes.tamas@gmail.com>
2015-07-24 11:06:13 +0200

    Implemented PID class

---
+++ Quadcopter/PID.ino
@@ -0,0 +1,34 @@
+class PID {
+public:
+  double pGain, iGain, dGain;
+  double pErr;
+  double integral;
+  
+  PID(double p, double i, double d){
+    pGain = p;
+    iGain = i;
+    dGain = d;
+    integral = 0;
+    pErr = 0;
+  }
+  
+  double compute(double err){
+    double toReturn = pGain*err + iGain*integral + dGain*(err-pErr);
+    integral += err;
+    pErr = err;
+    return toReturn;
+  }
+
+  double updateGains(double p, double i, double d){
+    pGain = p;
+    iGain = i;
+    dGain = d;
+  }
+
+  void reset(){
+    integral = 0;
+    pErr = 0;
+  }
+  
+};
+
