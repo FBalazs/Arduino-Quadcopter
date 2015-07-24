b89c4503f6515b656139d8003fb6cf1f379306b9
Balázs Fehér <fbalazs369@gmail.com>
2015-07-24 21:04:58 +0200

    CPPs

--- Quadcopter/Quadcopter.ino
+++ Quadcopter/Quadcopter.ino
@@ -4,6 +4,7 @@
 #include <MPU6050.h>
 
 #include <Servo.h>
+#include "PID.cpp"
 
 
 double pitch = 0,yaw = 0,roll = 0;
@@ -14,9 +15,9 @@ double ch_lift = 0, ch_pitch = 0, ch_yaw = 0, ch_roll = 0;
 double dt;
 long ctime, ptime, clocktime, lastcontroltime = -1;
 
-PID pidPitch = new PID(0,0,0);
-PID pidYaw = new PID(0,0,0);
-PID pidRoll = new PID(0,0,0);
+PID pidPitch = PID(0,0,0);
+PID pidYaw = PID(0,0,0);
+PID pidRoll = PID(0,0,0);
 
 
 void setup() {
