978d0bef6e86624fd80aa2c27e943c7d98bdb2a9
Tegzes Tamás <tegzes.tamas@gmail.com>
2016-04-21 17:44:04 +0200

    very significant improvement

--- Quadcopter/motor.h
+++ Quadcopter/motor.h
@@ -5,9 +5,9 @@
 
 class Motor{
 private:
-  static int MAX_POW = 250;
-  static int MID_POW = 80;
-  static int MIN_POW = 20;
+  static const int MAX_POW = 250;
+  static const int MID_POW = 80;
+  static const int MIN_POW = 20;
   int pin;
 public:
   Motor(int pin):pin(pin){}
